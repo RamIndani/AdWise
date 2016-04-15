833971418d0618f1bf92218b13bd43393aa5eeb5
RamIndani <ram.0737@gmail.com>
2016-04-15 14:43:39 -0700

    Added fragments to handle favorites, profile, seach and coupons

--- app/src/main/java/com/learninghorizon/adwise/home/HomeActivity.java
+++ app/src/main/java/com/learninghorizon/adwise/home/HomeActivity.java
@@ -1,15 +1,63 @@
 package com.learninghorizon.adwise.home;
 
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.learninghorizon.adwise.R;
+import com.learninghorizon.adwise.widget.view.SlidingTabLayout;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class HomeActivity extends AppCompatActivity implements ActionBar.TabListener{
+
+
+    @Bind(R.id.pager)
+    ViewPager mViewPager;
+   /* @Bind(R.id.toolbar)
+    Toolbar topToolBar;*/
+    private SlidingTabLayout mSlidingTabLayout;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        //setSupportActionBar(topToolBar);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(),getApplicationContext());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setCustomTabView(R.layout.custom_tab, R.id.item_title);
+        mSlidingTabLayout.setDistributeEvenly(true);
+        mSlidingTabLayout.setViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 }
---
+++ app/src/main/java/com/learninghorizon/adwise/home/SectionsPagerAdapter.java
@@ -0,0 +1,72 @@
+package com.learninghorizon.adwise.home;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.learninghorizon.adwise.R;
+import com.learninghorizon.adwise.home.favorites.FavoritesFragment;
+import com.learninghorizon.adwise.home.offers.OffersFragment;
+import com.learninghorizon.adwise.home.profile.ProfileFragment;
+import com.learninghorizon.adwise.home.search.SearchFragment;
+
+/**
+ * Created by ramnivasindani on 4/14/16.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+    Context mContext;
+    public SectionsPagerAdapter(FragmentManager fm, Context mContext) {
+        super(fm);
+        this.mContext = mContext;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new OffersFragment();
+            case 1:
+                return new FavoritesFragment();
+            case 2:
+               return new SearchFragment();
+            case 3:
+                return new ProfileFragment();
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    String title;
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                title = mContext.getString(R.string.offers);
+                break;
+            case 1:
+                title = mContext.getString(R.string.favorites);
+                break;
+            case 2:
+                title = mContext.getString(R.string.search);
+                break;
+            case 3:
+                title = mContext.getString(R.string.profile);
+                break;
+
+            default:
+                break;
+        }
+
+        return title;
+    }
+}
